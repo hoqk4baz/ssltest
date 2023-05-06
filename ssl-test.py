49fbbc46ed8a433693a2b6c7e63dd3a812cafebd
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2023-05-06 16:56:35 +0300

    Create ssl-test.py

---
+++ ssl-test.py
@@ -0,0 +1,41 @@
+import subprocess
+import time
+
+M = "\033[34m"
+Y = "\033[32m"
+R = "\033[31m"
+
+print(M+"  _____             _      ______                "+M)
+print(M+" |  __ \           | |    |  ____|               "+M)
+print(M+" | |  | | __ _ _ __| | __ | |__   _ __  ______ _ "+M)
+print(M+" | |  | |/ _` | '__| |/ / |  __| | '_ \|_  / _` |"+M)
+print(M+" | |__| | (_| | |  |   <  | |____| | | |/ / (_| |"+M)
+print(M+" |_____/ \__,_|_|  |_|\_\ |______|_| |_/___\__,_|"+M)
+print(R+" ‎Merhaba! Ben Python kullanıyorum.  TG: dark_enza"+R)
+print("")
+print(Y+" İnternetsiz SSL/SNI Test Aracı"+Y)
+print(R+" Telegram Kanalım : @dwstoree"+R)
+
+
+while True:
+    secimyap = input(M+'[1]Hostu Elle Gir\n[2]Hostu Host.txt Dosyasından Çek\n[3]Çıkış\nSeçiminiz: '+M)
+
+    if secimyap == '1':
+        hostlar = input('Hostu Gir (Çoklu ise boşluk ile ayırın): ').split()
+        break
+    elif secimyap == '2':
+        with open('host.txt', 'r') as f:
+            hostlar = f.read().splitlines()
+        break
+    elif secimyap == '3':
+        exit()
+    else:
+        print(R+'Yanlış seçim yaptınız. Tekrar deneyin.'+R)
+        time.sleep(2)
+
+for host in hostlar:
+    sonuc = subprocess.run(['timeout', '0.5', 'openssl', 's_client', '-connect', 'ssl.dark-enza.club:443', '-servername', host], capture_output=True, text=True)
+    if 'CONNECTED' in sonuc.stdout:
+        print(Y+f'{host}: BAĞLANTI BAŞARILI'+Y)
+    else:
+        print(R+f'{host}: BAĞLANTI BAŞARISIZ'+R)
