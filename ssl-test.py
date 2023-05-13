e16cea0d34ad7977444f0b82781fd44699884b33
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2023-05-13 11:22:21 -0700

    Update ssl-test.py

--- ssl-test.py
+++ ssl-test.py
@@ -5,23 +5,23 @@ M = "\033[34m"
 Y = "\033[32m"
 R = "\033[31m"
 
-print(M+"  _____             _      ______                "+M)
-print(M+" |  __ \           | |    |  ____|               "+M)
-print(M+" | |  | | __ _ _ __| | __ | |__   _ __  ______ _ "+M)
-print(M+" | |  | |/ _` | '__| |/ / |  __| | '_ \|_  / _` |"+M)
-print(M+" | |__| | (_| | |  |   <  | |____| | | |/ / (_| |"+M)
-print(M+" |_____/ \__,_|_|  |_|\_\ |______|_| |_/___\__,_|"+M)
-print(R+" ‎Merhaba! Ben Python kullanıyorum.  TG: dark_enza"+R)
+print(M+"  _____             _      ______                "+M)
+print(M+" |  __ \           | |    |  ____|               "+M)
+print(M+" | |  | | __ _ _ __| | __ | |__   _ __  ______ _ "+M)
+print(M+" | |  | |/ _` | '__| |/ / |  __| | '_ \|_  / _` |"+M)
+print(M+" | |__| | (_| | |  |   <  | |____| | | |/ / (_| |"+M)
+print(M+" |_____/ \__,_|_|  |_|\_\ |______|_| |_/_/ \__,_|"+M)
+print(R+" ‎Merhaba! Ben Python kullanıyorum.  TG: dark_enza"+R)
 print("")
-print(Y+" İnternetsiz SSL/SNI Test Aracı"+Y)
-print(R+" Telegram Kanalım : @dwstoree"+R)
+print(Y+" Internetsiz SSL/SNI Test Araci"+Y)
+print(R+" Telegram Kanalim : @dwstoree"+R)
 
 
 while True:
-    secimyap = input(M+'[1]Hostu Elle Gir\n[2]Hostu Host.txt Dosyasından Çek\n[3]Çıkış\nSeçiminiz: '+M)
+    secimyap = input(M+'[1] Hostu elle gir\n[2] Hostu host.txt dosyasindan al\n[3] Cikis\nSeciminiz: '+M)
 
     if secimyap == '1':
-        hostlar = input('Hostu Gir (Çoklu ise boşluk ile ayırın): ').split()
+        hostlar = input('Hostu gir (Coklu ise bosluk ile ayirin): ').split()
         break
     elif secimyap == '2':
         with open('host.txt', 'r') as f:
@@ -30,7 +30,7 @@ while True:
     elif secimyap == '3':
         exit()
     else:
-        print(R+'Yanlış seçim yaptınız. Tekrar deneyin.'+R)
+        print(R+'Yanlis secim yaptiniz. Tekrar deneyin.'+R)
         time.sleep(2)
 
 with open('baglananlar.txt', 'w') as baglananlar:
@@ -38,9 +38,11 @@ with open('baglananlar.txt', 'w') as baglananlar:
 
 with open('baglananlar.txt', 'a') as baglananlar:
     for host in hostlar:
-        sonuc = subprocess.run(['timeout', '1.3', 'openssl', 's_client', '-connect', 'ssl.dark-enza.club:443', '-servername', host], capture_output=True, text=True)
+        sonuc = subprocess.run(['openssl', 's_client', '-connect', 'ssl.dark-enza.club:443', '-servername', host], capture_output=True, text=True, timeout=2)
+
+        print(sonuc)
         if 'CONNECTED' in sonuc.stdout:
-            print(Y+f'{host}: BAĞLANTI BAŞARILI'+Y)
+            print(Y+f'{host}: BAGLANTI BASARILI'+Y)
             baglananlar.write(f'{host}\n')
         else:
-            print(R+f'{host}: BAĞLANTI BAŞARISIZ'+R)
+            print(R+f'{host}: BAGLANTI BASARISIZ'+R)
